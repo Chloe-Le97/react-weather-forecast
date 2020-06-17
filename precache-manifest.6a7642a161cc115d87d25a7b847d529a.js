41b6a9496d03c7279989edfa467b0abab1f3a27c
cat-isme <phuonganh1902.le@gmail.com>
2020-06-18 03:25:32 +0700

    Updates

---
+++ precache-manifest.6a7642a161cc115d87d25a7b847d529a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f81cbd35e6f365f5f4581b2904a4b3",
+    "url": "/react-weather-forecast/index.html"
+  },
+  {
+    "revision": "5be5ea2957345e1f855c",
+    "url": "/react-weather-forecast/static/css/2.8e23b477.chunk.css"
+  },
+  {
+    "revision": "2998e7c417ffd5740b6e",
+    "url": "/react-weather-forecast/static/css/main.89c152b1.chunk.css"
+  },
+  {
+    "revision": "5be5ea2957345e1f855c",
+    "url": "/react-weather-forecast/static/js/2.8230d315.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-forecast/static/js/2.8230d315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2998e7c417ffd5740b6e",
+    "url": "/react-weather-forecast/static/js/main.3056569b.chunk.js"
+  },
+  {
+    "revision": "3ca32a08654b979340e4",
+    "url": "/react-weather-forecast/static/js/runtime-main.56dd05c9.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/react-weather-forecast/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/react-weather-forecast/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/react-weather-forecast/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/react-weather-forecast/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/react-weather-forecast/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
